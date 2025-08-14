18c9bb01c42ca8ed2bf7aa03cf574922ca1b17d9
Annibelle Boling <annibelle.boling@gmail.com>
2025-08-14 00:38:45 -0600

    Initial commit

---
+++ spec/late_template_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe LateTemplate do
+  it "has a version number" do
+    expect(LateTemplate::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
